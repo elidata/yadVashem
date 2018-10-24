4f0652b312a4b769fc99bde5298be03019c35927
Orisadek <ori.sadek@gmail.com>
2018-10-24 14:59:53 +0300

    new photos

--- src/GoTo.js
+++ src/GoTo.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import * as distancesJson from './distances.json';
 import { Link } from 'react-router-dom';
-import {Navbar} from './Navbar';
-import {Footer} from './Footer';
+import { Navbar } from './Navbar';
+import { Footer } from './Footer';
 
 export class GoTo extends Component {
     constructor(props) {
@@ -36,14 +36,14 @@ export class GoTo extends Component {
     render() {
         return (
             <div dir={this.props.direction} >
-            <Navbar languageNow={this.props.languageNow}/>
+                <Navbar languageNow={this.props.languageNow} />
                 <ul>
                     {/* do a map on the keys and according the URL choose which places to display */}
                     {this.state.chosenDistance.map(
                         (item, i) =>
                             (window.location.href.substr(window.location.href.lastIndexOf('/') + 1) !== this.state.chosenDistance[i].name &&
                                 <Link to={`/${this.props.languageNow.path}/info/${this.state.chosenDistance[i].name}`}>
-                                    <li className = "goToList" key={i} value={item}>{Object.keys(this.locationsList).map((location, index) =>
+                                    <li className="goToList" key={i} value={item}>{Object.keys(this.locationsList).map((location, index) =>
                                         (location === this.state.chosenDistance[i].name && this.locationsList[location].label))}
                                         &nbsp;
                                      {this.state.chosenDistance[i].distance}</li>
@@ -52,7 +52,7 @@ export class GoTo extends Component {
                             )
                     )}
                 </ul>
-              <Footer  languageNow={this.props.languageNow} dir = {this.props.direction}/>
+                <Footer languageNow={this.props.languageNow} dir={this.props.direction} />
 
             </div>
         );
